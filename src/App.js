89b2526150575c1a1f24b7161ae8458e3f7d982c
Olivia Thetgyi <othetgyi@gmail.com>
2021-10-29 17:37:58 +0100

    Add TypeScript and @types/react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=20&offset=200`
+      );
+      console.log(result);
+    };
+  });
+
   return <div className="container">Hello</div>;
 };
 
---
+++ src/components/PokemonCharacter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const PokemonCharacter = () => {
+  return (
+    <div>
+      <h2>Pikachu</h2>
+      <div>Type: Grass </div>
+      <div>Height:</div>
+      <div>Weight:</div>
+      <div>Abilities</div>
+    </div>
+  );
+};
