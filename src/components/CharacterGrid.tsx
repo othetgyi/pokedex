d6c04cc983edbc2ac7be804a9adb9bae35dda6a8
Olivia Thetgyi <othetgyi@gmail.com>
2021-12-08 19:46:51 +0000

    add styling to cards

--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Character } from "../App";
+import styled from "styled-components";
 import { CharacterItem } from "./CharacterItem";
+
+const CardsGrid = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 1rem;
+  margin: 0 auto;
+  max-width: 960px;
+`;
 
 export const CharacterGrid: React.FC<{ characters: Character[] }> = ({
   characters,
 }) => {
   return (
-    <div>
+    <CardsGrid>
       {characters.map((character: any) => (
         <CharacterItem character={character} />
       ))}
-    </div>
+    </CardsGrid>
   );
 };
--- src/components/CharacterItem.tsx
+++ src/components/CharacterItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 interface Character {
   sprites: {
     other: {
@@ -10,17 +11,34 @@ interface Character {
   name: string;
 }
 
+const StyledCard = styled.div`
+  display: grid;
+  background-color: #f5f5f5;
+  font-size: 20px;
+  font-family: "Gill Sans", sans-serif;
+  height: 200px;
+  padding: 5px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+const PokemonImage = styled.img`
+  width: auto;
+  max-height: 150px;
+`;
+
 export const CharacterItem: React.FC<{ character: Character }> = ({
   character,
 }) => {
   console.log("***character***", character);
   return (
-    <div>
-      <img
+    <StyledCard>
+      <PokemonImage
         src={character.sprites.other.dream_world.front_default}
         alt={character.name}
       />
       <h3>{character.name}</h3>
-    </div>
+    </StyledCard>
   );
 };
