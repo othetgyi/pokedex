4c6f73b590d6999b142075eb3fabdcd769fde33b
Olivia Thetgyi <othetgyi@gmail.com>
2022-10-21 17:04:06 +0100

    Add unit tests for StatisticsCard component

--- src/ui/components/StatisticsCard.test.tsx
+++ src/ui/components/StatisticsCard.test.tsx
@@ -3,13 +3,15 @@ import { StatisticsCard } from "./StatisticsCard";
 
 describe("StatisticsCard", () => {
     it("displays the Pokemon genus", () => {
-        render(<StatisticsCard />);
+        render(<StatisticsCard height={10} weight={5} genus="mockGenus"/>);
         expect(screen.getByText("mockGenus")).toBeVisible();
     });
     it("displays the Pokemon's height", () => {
-
+        render(<StatisticsCard height={10} weight={20} genus="mockGenus"/>);
+        expect(screen.getByText('1m')).toBeVisible();
     });
     it("displays the Pokemon's weight", () => {
-
+        render(<StatisticsCard height={10} weight={20} genus="mockGenus"/>);
+        expect(screen.getByText('2kg')).toBeVisible();
     })
 })
